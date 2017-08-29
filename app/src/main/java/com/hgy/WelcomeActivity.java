4a6d67fa3f1e7d4b0f143567c8fdd923f2b8fa50
liurentian <972377892@qq.com>
2017-08-29 17:02:24 +0800

    欢迎界面 完成

--- app/src/main/java/com/hgy/WelcomeActivity.java
+++ app/src/main/java/com/hgy/WelcomeActivity.java
@@ -1,21 +1,32 @@
 package com.hgy;
 
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hgy.main.MainActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActionBar actionBar=getSupportActionBar();
-        actionBar.hide();
+
         setContentView(R.layout.activity_welcome);
         initDate();
     }
 
     private void initDate() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ToHome();
+            }
+        },1000);
+    }
 
+    private void ToHome() {
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
---
+++ app/src/main/java/com/hgy/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hgy.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hgy.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
